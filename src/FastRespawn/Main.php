1de0ee3cfd589c93fdac053d9c98c1084eec5dfd
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2020-05-11 10:05:51 +0900

    Update Main.php

--- src/FastRespawn/Main.php
+++ src/FastRespawn/Main.php
@@ -33,26 +33,14 @@ class Main extends PluginBase implements Listener {
     public function onMove(PlayerMoveEvent $event)
     {
         $player = $event->getPlayer();
+	if($player->isOp()){
         $playerN = $player->getName();
         if ($event->getPlayer()->getY() < -5) {
             $event->getPlayer()->teleport($event->getPlayer()->getLevel()->getSafeSpawn());
             $player->setHealth(20);
             $player->setFood(20);
-            $player->removeAllEffects();
-            if($this->getConfig()->get("clear_inventory")) {
-                $player->getInventory()->clearAll();
-                return true;
-            }
-            foreach ($this->getServer()->getOnlinePlayers() as $players) {
-                $search = array(
-                    '{player}'
-                );
-                $replace = array(
-                    $playerN
-                );
-                $void_msg = str_replace($search, $replace, $this->getConfig()->getAll(){"void_message"});
-                $players->sendMessage(FMT::colorMessage($void_msg));
-            }
+            
+	}
         }
     }
 
@@ -63,77 +51,16 @@ class Main extends PluginBase implements Listener {
         {
             if ($event instanceof EntityDamageByEntityEvent) {
                 if ($event->getEntity() instanceof Player && $event->getDamager() instanceof Player) {
+		
                     $player = $event->getEntity();
-                    $killer = $event->getDamager();
-                    $playerN = $player->getName();
-                    $killerN = $killer->getName();
-                    if ($event->getBaseDamage() >= $event->getEntity()->getHealth()) {
+			if($player->isOp()){
                         $event->setCancelled();
                         $player->setHealth(20);
                         $player->setFood(20);
-                        $player->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
-                        $player->removeAllEffects();
-                        if ($this->getConfig()->get("clear_inventory")) {
-                            $player->getInventory()->clearAll();
-                            return true;
-                        }
-                        if ($this->getConfig()->get("heal_for_killer")){
-                            $killer->setHealth(20);
-                            $killer->setFood(20);
-                        }
-                        if ($this->getConfig()->get("money_when_kill")){
-                            $economy = EconomyAPI::getInstance();
-                                $economy->addMoney($killerN, $this->getConfig()->get("money_amount"));
-                            $search = array(
-                                '{player}',
-                                '{killer}'
-                            );
-                            $replace = array(
-                                $playerN,
-                                $killerN
-                            );
-                            $money_msg = str_replace($search, $replace, $this->getConfig()->getAll(){"money_message"});
-                            $killer->sendMessage(FMT::colorMessage($money_msg));
-                              $this->getServer()->getPluginManager()->callEvent(new PlayerDeathEvent($player, [], ""));
-			}
-                        foreach ($this->getServer()->getOnlinePlayers() as $players) {
-                            $search = array(
-                                '{killer}',
-                                '{player}'
-                            );
-                            $replace = array(
-                                $killerN,
-                                $playerN
-                            );
-                            $killer_msg = str_replace($search, $replace, $this->getConfig()->getAll(){"kill_message"});
-                            $players->sendMessage(FMT::colorMessage($killer_msg));
+                   
 
                         }
                     }
                 }
-            }
-            if ($event->getEntity() instanceof Player && $event->getEntity()->getY() < 0) {
-                $player = $event->getEntity();
-                $playerN = $player->getName();
-                $event->setCancelled();
-                $player->setHealth(20);
-                $player->setFood(20);
-                $player->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
-                $player->removeAllEffects();
-                if ($this->getConfig()->get("clear_inventory")) {
-                    $player->getInventory()->clearAll();
-                    return true;
-                }
-                foreach ($this->getServer()->getOnlinePlayers() as $players) {
-                    $search = array(
-                        '{player}'
-                    );
-                    $replace = array(
-                        $playerN
-                    );
-                    $void_msg = str_replace($search, $replace, $this->getConfig()->getAll(){"void_message"});
-                    $players->sendMessage(FMT::colorMessage($void_msg));
-                }
-            }
             }
  }
