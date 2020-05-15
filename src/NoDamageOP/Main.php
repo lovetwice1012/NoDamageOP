fcfed6f0811f9c89af3f1caaec46db6dd831593c
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2020-05-15 16:06:43 +0900

    インデントの修正

--- src/NoDamageOP/Main.php
+++ src/NoDamageOP/Main.php
@@ -17,45 +17,47 @@ use pocketmine\event\entity\EntityEffectAddEvent;
 
 class Main extends PluginBase implements Listener {
 
-    private $players = [];
+	private $players = [];
 
-	public function onEnable()
-	{
+	public function onEnable(){
 		$this->getLogger()->info(Color::AQUA . "NoDamageOP Enabled By @lovetwice1012");
-                $this->getServer()->getPluginManager()->registerEvents($this ,$this);
+        	$this->getServer()->getPluginManager()->registerEvents($this ,$this);
         }
 	
+	
         public function onMove(PlayerMoveEvent $event): void {
-            $player = $event->getPlayer();
-            if(!$player->isOp()){
-                return;
-            }
-            if($player->y < 0) {
-                $player->setHealth(20);
-                $player->setFood(20);
-                $player->teleport($player->getLevel()->getSafeSpawn());
-    	        $this->getLogger()->info("§a".$player->getName()."が奈落に落ちそうになったためリスポーン地点にワープさせました。");
-	        $player->sendTip("§a".$player->getName()."が奈落に落ちそうになったためリスポーン地点にワープさせました。");
-            }
+        	$player = $event->getPlayer();
+        	if(!$player->isOp()){
+           		return;
+            	}
+            	if($player->y < 0) {
+                	$player->setHealth(20);
+                	$player->setFood(20);
+               		$player->teleport($player->getLevel()->getSafeSpawn());
+    	        	$this->getLogger()->info("§a".$player->getName()."が奈落に落ちそうになったためリスポーン地点にワープさせました。");
+	        	$player->sendTip("§a".$player->getName()."が奈落に落ちそうになったためリスポーン地点にワープさせました。");
+            	}
         }
 
 
         public function onDamage(EntityDamageEvent $event){
-            $player = $event->getEntity();
-            if(!$player->isOp()){
-                return;
-	    }
-            $event->setCancelled();
-            $player->setHealth(20);
-            $player->setFood(20);
-	    $this->getLogger()->info("§a".$player->getName()."に与えられた§6".$event->getBaseDamage()."§aダメージを無効化しました。");
-            $player->sendTip("§a".$player->getName()."に与えられた§6".$event->getBaseDamage()."§aダメージを無効化しました。");
+            	$player = $event->getEntity();
+            	if(!$player->isOp()){
+                	return;
+	    	}
+            	$event->setCancelled();
+            	$player->setHealth(20);
+            	$player->setFood(20);
+	    	$this->getLogger()->info("§a".$player->getName()."に与えられた§6".$event->getBaseDamage()."§aダメージを無効化しました。");
+            	$player->sendTip("§a".$player->getName()."に与えられた§6".$event->getBaseDamage()."§aダメージを無効化しました。");
          }
+	
+	
 	public function  onEffectAdded(EntityEffectAddEvent $event) : void {
-            if($event->getEntity() instanceof Player && $event->getEffect()->getType()->isBad() && $event->getEntity()->isOp()){
-                $event->setCancelled();
-		$this->getLogger()->info("§a".$event->getEntity()->getName()."に与えられた§6".$event->getEffect()->getType()->getName()."§エフェクトを無効化しました。");
-		$event->getEntity()->sendTip("§a".$event->getEntity()->getName()."に与えられた§6".$event->getEffect()->getType()->getName()."§aエフェクトを無効化しました。");
-             }
+            	if($event->getEntity() instanceof Player && $event->getEffect()->getType()->isBad() && $event->getEntity()->isOp()){
+                	$event->setCancelled();
+			$this->getLogger()->info("§a".$event->getEntity()->getName()."に与えられた§6".$event->getEffect()->getType()->getName()."§エフェクトを無効化しました。");
+			$event->getEntity()->sendTip("§a".$event->getEntity()->getName()."に与えられた§6".$event->getEffect()->getType()->getName()."§aエフェクトを無効化しました。");
+             	}
         }
  }
