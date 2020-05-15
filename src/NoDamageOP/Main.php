63933a113188d113bfd4f27d29f2d676e73dec21
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2020-05-15 16:08:18 +0900

    色文字が抜けてた

--- src/NoDamageOP/Main.php
+++ src/NoDamageOP/Main.php
@@ -56,7 +56,7 @@ class Main extends PluginBase implements Listener {
 	public function  onEffectAdded(EntityEffectAddEvent $event) : void {
             	if($event->getEntity() instanceof Player && $event->getEffect()->getType()->isBad() && $event->getEntity()->isOp()){
                 	$event->setCancelled();
-			$this->getLogger()->info("§a".$event->getEntity()->getName()."に与えられた§6".$event->getEffect()->getType()->getName()."§エフェクトを無効化しました。");
+			$this->getLogger()->info("§a".$event->getEntity()->getName()."に与えられた§6".$event->getEffect()->getType()->getName()."§aエフェクトを無効化しました。");
 			$event->getEntity()->sendTip("§a".$event->getEntity()->getName()."に与えられた§6".$event->getEffect()->getType()->getName()."§aエフェクトを無効化しました。");
              	}
         }
